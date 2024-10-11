e613525bd7cfbc386602a0e354bac40167ab4c1d
DoctorPozdnyakov <svo@svo.ru>
2024-10-11 18:20:09 +0000

    Programm completed all tests

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -2,6 +2,25 @@
 #include "str2int.h"
 #include "stdio.h"
 
+
 int str2int(const char *str) {
-    return 0;
+    int sign = 1;
+    if (*str == 45){
+        sign = -1;
+        str++;
+    }
+    if (*str == 43){
+        str++;
+    }
+    int result = 0;
+    int count = 0;
+    while (*str != '\0'){
+        result = result * 10 + (*str - *"0");
+        count += 1;
+        str++;
+        assert(sign == -1 && result == -2147483648 || sign == -1 && result >= 0 && count <= 9 || sign == 1 && result >= 0);
+
+    }
+    assert(count > 0);
+    return sign * result;
 }
