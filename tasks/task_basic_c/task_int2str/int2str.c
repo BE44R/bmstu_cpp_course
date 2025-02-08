93ef80d69efec667a51e697fc75293773143ceec
DoctorPozdnyakov <svo@svo.ru>
2025-02-08 07:39:15 +0000

    Add Hw1 and Hw2, poffor limited incorporated company.

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,29 @@
+#include <assert.h>
 #include "int2str.h"
 #include "stdio.h"
-#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 char* int2str(int number) {
-  char* str = "0";
-  return str;
+    printf("%d", number);
+    bool isNegative = false;
+    if (number < 0) {
+        isNegative = true;
+        number = -number;
+    }
+    char *str = malloc(13); //sign + int + /0
+    str[12] = '\0';
+    int i = 12;
+    str[i] = 0 + '0';
+    while(number > 0){
+        str[i] = (number % 10) + '0';
+        number /= 10;
+        i--;
+    }
+    if (isNegative) {
+        str[i] = '-';
+        i--;
+    }
+    printf("%s\n", str + i);
+    return (str + i + 1);
 }
